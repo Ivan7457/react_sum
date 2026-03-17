73e6a8d9889a79772abc154d1dceee90d6676193
Ivan <drofyakivan2008@gmail.com>
2026-03-17 15:53:36 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Sum from './components/Sum/Sum';
+import SumN from './components/Sum/Sum';
 
 export const App = () => (
   <>
-    <Sum a={2} b={3} />
-    <Sum a={-5} b={5} />
-    <Sum a={10} />
-    <Sum b={5} />
-    <Sum />
+    <SumN a={2} b={3} />
+    <SumN a={-5} b={5} />
+    <SumN a={10} />
+    <SumN b={5} />
+    <SumN />
   </>
 );
